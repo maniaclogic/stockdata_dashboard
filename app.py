77f248a19e2cbe9bb4087bfee5eb2e9622b395ce
Kat <katjabensiek@gmail.com>
2019-05-17 16:51:32 +0200

    css stylesheet implementation

--- app.py
+++ app.py
@@ -13,24 +13,33 @@ start = datetime.datetime.today() - relativedelta(years=5)
 end = datetime.datetime.today()
 
 df = get_historical_data('DAX', start=start, end=end, output_format='pandas', token=public_token)
+df1 = get_historical_data('AMZN', start=start, end=end, output_format='pandas', token=public_token)
 
 trace_close = go.Scatter(x=list(df.index),
                          y=list(df.close),
                          name='Close',
                          line=dict(color='#006666'))
+trace_close1 = go.Scatter(x=list(df1.index),
+                         y=list(df1.close),
+                         name='Close',
+                         line=dict(color='#006666'))
 
 data = [trace_close]
+data1 = [trace_close1]
 
 layout = dict(title='DAX chart',
               showlegend=False)
+layout1 = dict(title='Amazon chart',
+              showlegend=False)
 
 fig = dict(data=data, layout=layout)
+fig1 = dict(data=data1, layout=layout1)
 
 app = dash.Dash()
 
 app.layout = html.Div([
     html.H1(children='Stock Data Dashboard'),
-    html.Label('DAX Chart'),
+    html.Label('Financial Stock Charting'),
     html.Div(
         dcc.Input(
             id='stock-input',
@@ -49,11 +58,21 @@ app.layout = html.Div([
         )
     ),
 
-    html.Div(
-        dcc.Graph(id='DAX chart',
+    html.Div([
+        html.Div([
+            dcc.Graph(id='DAX chart',
                   figure=fig)
-    )
+        ], className='six columns'),
+        html.Div([
+            dcc.Graph(id='Amazon chart',
+                      figure=fig1)
+        ], className='six columns')
+    ], className='row')
 ])
+
+app.css.append_css({
+    'external_url':'https://codepen.io/chriddyp/pen/bWLwgP.css'
+})
 
 if __name__=="__main__":
     app.run_server(debug=True)
