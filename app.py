78c611619623a34a598c037debec7d01e638231f
Kat <katjabensiek@gmail.com>
2019-05-17 19:58:37 +0200

    Call back function error during html integration

--- app.py
+++ app.py
@@ -1,51 +1,50 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import iexfinance as finance
+from dash.dependencies import Output, Input
 from iexfinance.stocks import get_historical_data
 import datetime
 from dateutil.relativedelta import relativedelta
 import plotly.graph_objs as go
 
-public_token = 'pk_027ce70b2a994d169bcac0a0da2fdcdd'
+public_token = 'pk_ca63b49cdbb34875ab21176ec50e99c6'
 
-start = datetime.datetime.today() - relativedelta(years=5)
+start = datetime.datetime.today() - relativedelta(days=5)
 end = datetime.datetime.today()
 
-df = get_historical_data('DAX', start=start, end=end, output_format='pandas', token=public_token)
 df1 = get_historical_data('AMZN', start=start, end=end, output_format='pandas', token=public_token)
 
-trace_close = go.Scatter(x=list(df.index),
-                         y=list(df.close),
-                         name='Close',
-                         line=dict(color='#006666'))
+
+
 trace_close1 = go.Scatter(x=list(df1.index),
                          y=list(df1.close),
                          name='Close',
                          line=dict(color='#006666'))
 
-data = [trace_close]
 data1 = [trace_close1]
 
-layout = dict(title='DAX chart',
-              showlegend=False)
+
 layout1 = dict(title='Amazon chart',
               showlegend=False)
 
-fig = dict(data=data, layout=layout)
+
 fig1 = dict(data=data1, layout=layout1)
+
+
 
 app = dash.Dash()
 
 app.layout = html.Div([
-    html.H1(children='Stock Data Dashboard'),
+    html.Div([
+        html.H2('Stock Data Dashboard'),
+        html.Img(src='/assets/stock-icon.png'),
+        ], className='banner'),
     html.Label('Financial Stock Charting'),
     html.Div(
         dcc.Input(
             id='stock-input',
-            placeholder='Enter a Stock to get tickers',
             type='text',
-            value=''
+            value='SPY'
         ),
     ),
 
@@ -60,8 +59,7 @@ app.layout = html.Div([
 
     html.Div([
         html.Div([
-            dcc.Graph(id='DAX chart',
-                  figure=fig)
+            dcc.Graph(id='chart',)
         ], className='six columns'),
         html.Div([
             dcc.Graph(id='Amazon chart',
@@ -73,6 +71,24 @@ app.layout = html.Div([
 app.css.append_css({
     'external_url':'https://codepen.io/chriddyp/pen/bWLwgP.css'
 })
+
+@app.callback(dash.dependencies.Output('chart', 'figure'),
+                  [dash.dependencies.Input('stock-input', 'value')])
+
+def update_fig(input_value):
+    df = get_historical_data(input_value, start=start, end=end, output_format='pandas', token=public_token)
+
+    data =[]
+    trace_close = go.Scatter(x=list(df.index),
+                             y=list(df.close),
+                             name='Close',
+                             line=dict(color='#006666'))
+    data.append(trace_close)
+    layout = dict(title='Callback_Graph',
+                  showlegend=False)
+    value = {'data': data, 'layout': layout}
+
+    return value
 
 if __name__=="__main__":
     app.run_server(debug=True)
